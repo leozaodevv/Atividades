99bc8b611b84c471d2acd92af133a296b07ff6f8
leozaodevv <leonardojsantoos@gmail.com>
2025-11-27 15:57:49 -0300

    Atividade 27.11

---
+++ ProjetoIMC/ProjetoIMC/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insira o peso: (em Kg | ex: 70)");
+double pesoEmKg = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Insira a altura: (em Metros | ex: 1,70)");
+double alturaEmMetros = Convert.ToDouble(Console.ReadLine());
+
+if (alturaEmMetros == 0)
+{
+    Console.WriteLine("A altura deve ser maior que zero.");
+    return;
+}
+
+double alturaAoQuadrado = alturaEmMetros * alturaEmMetros;
+double imc = pesoEmKg / alturaAoQuadrado;
+double imcFormatado = Math.Round(imc, 2);
+
+string sit = "";
+
+if (imcFormatado < 18.5)
+{
+    sit = "Abaixo do Peso";
+}
+else if (imcFormatado >= 18.5 && imcFormatado <= 24.9)
+{
+    sit = "Normal";
+}
+else if (imcFormatado >= 25 && imcFormatado <= 29.9)
+{
+    sit = "Sobre Peso";
+}
+else
+{
+    sit = "Obesidade";
+}
+
+Console.WriteLine($"O IMC calculado é: {imcFormatado} | Situação: {sit}.");
+
---
+++ ProjetoIMC/ProjetoINSS/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insira seu salario: ");
+double sal = Convert.ToDouble(Console.ReadLine());
+double des = 0;
+int fx = 0;
+if(sal <= 1100)
+{
+    des = sal * 0.075;
+    fx = 1;
+}
+else if(sal >= 1100.10 && sal <= 2203.48){
+    des = sal * 0.09;   
+    fx = 2;
+}
+else if (sal >= 2203.49 && sal <= 3305.22)
+{
+    des = sal * 0.12;
+    fx = 3;
+}
+else if (sal >= 3305.23)
+{
+    des = sal * 0.14;
+    fx = 4;
+}
+Console.WriteLine($"Salario: R${Math.Round(sal,2)} | Contribuição INSS: R${Math.Round(des, 2)} | Faixa: {fx}");
+
+
