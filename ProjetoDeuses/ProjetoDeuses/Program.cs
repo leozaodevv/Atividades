36a729acb5cbbcc075fe41fc777b6851efb1ae89
leozaodevv <leonardojsantoos@gmail.com>
2025-12-01 15:38:57 -0300

    Mudança de varios IFs para switch case

--- ProjetoDeuses/ProjetoDeuses/Program.cs
+++ ProjetoDeuses/ProjetoDeuses/Program.cs
@@ -11,31 +11,33 @@ if (res == 's')
         Console.Clear();
         Console.WriteLine("Qual é um dos poderes desse deus?\n\n1 - Raio\n2 - Água\n3 - Guerra");
         res = Convert.ToChar(Console.ReadLine());
-        if(res == '1')
+        switch (res)
         {
-            Console.Clear();
-            Console.WriteLine("Esse deus é ZEUS.");
-        }
-        else if(res == '2')
-        {
-            Console.Clear();
-            Console.WriteLine("Esse deus é POSEIDON.");
-        }
-        else if (res == '3')
-        {
-            Console.Clear();
-            Console.WriteLine("Em qual sentido essa Guerra se encaixa mais:\n\n1 - Bruta, selvagem e destruição\n2 - Estratégica, inteligência militar e táticas ");
-            res = Convert.ToChar(Console.ReadLine());   
-            if(res == '1')
-            {
+            case '1':
                 Console.Clear();
-                Console.WriteLine("Esse deus é ARES.");
-            }
-            else if(res == '2')
-            {
+                Console.WriteLine("Esse deus é ZEUS.");
+                break;
+
+            case '2':
                 Console.Clear();
-                Console.WriteLine("Essa deusa é Atena");
-            }
+                Console.WriteLine("Esse deus é POSEIDON.");
+                break;
+
+            case '3':
+                Console.Clear();
+                Console.WriteLine("Em qual sentido essa Guerra se encaixa mais:\n\n1 - Bruta, selvagem e destruição\n2 - Estratégica, inteligência militar e táticas");
+                res = Convert.ToChar(Console.ReadLine());
+                if (res == '1')
+                {
+                    Console.Clear();
+                    Console.WriteLine("Esse deus é ARES.");
+                }
+                else if (res == '2')
+                {
+                    Console.Clear();
+                    Console.WriteLine("Essa deusa é ATENA");
+                }
+                break;
         }
     }
     else if(res == 'n')
@@ -43,93 +45,100 @@ if (res == 's')
         Console.Clear();
         Console.WriteLine("Local onde vive:\n\n1 - Submundo\n2 - Natureza\n3 - Céu");
         res = Convert.ToChar(Console.ReadLine());
-        if(res == '1')
+        switch (res)
         {
-            Console.Clear();
-            Console.WriteLine("Controla o Submundo? s/n");
-            res = Convert.ToChar(Console.ReadLine());
-            if(res == 's')
-            {
+            case '1':
                 Console.Clear();
-                Console.WriteLine("Esse deus é HADES.");
-            }
-            else if (res == 'n')
-            {
-                Console.Clear();
-                Console.WriteLine("Ao que esse deus é associado?\n\n1 - Noite\n2 - Morte\n3 - Escuridão");
+                Console.WriteLine("Controla o Submundo? s/n");
                 res = Convert.ToChar(Console.ReadLine());
-                if (res == '1')
+                if (res == 's')
                 {
                     Console.Clear();
-                    Console.WriteLine("Esse deus é NIX.");
+                    Console.WriteLine("Esse deus é HADES.");
                 }
-                else if(res == '2')
+                else if (res == 'n')
                 {
                     Console.Clear();
-                    Console.WriteLine("Esse deus esta mais ligado à:\n\n1 - Necromancia, magia\n2 - Personificação da Morte");
+                    Console.WriteLine("Ao que esse deus é associado?\n\n1 - Noite\n2 - Morte\n3 - Escuridão");
                     res = Convert.ToChar(Console.ReadLine());
-                    if(res == '1')
+                    switch (res)
                     {
-                        Console.Clear();
-                        Console.WriteLine("Essa deusa é HÉCATE.");
-                    }
-                    else if (res == '2')
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Esse deus é TÂNATO.");
+                        case '1':
+                            Console.Clear();
+                            Console.WriteLine("Esse deus é NIX.");
+                            break;
+
+                        case '2':
+                            Console.Clear();
+                            Console.WriteLine("Esse deus esta mais ligado à:\n\n1 - Necromancia, magia\n2 - Personificação da Morte");
+                            res = Convert.ToChar(Console.ReadLine());
+                            if (res == '1')
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Essa deusa é HÉCATE.");
+                            }
+                            else if (res == '2')
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Esse deus é TÂNATO.");
+                            }
+                            break;
+
+                        case '3':
+                            Console.Clear();
+                            Console.WriteLine("Esse deus é ÉREBO.");
+                            break;
                     }
                 }
-                else if(res == '3')
+                break;
+
+            case '2':
+                Console.Clear();
+                Console.WriteLine("Esse deus esta mais associado à:\n\n1 - Florestas e montanhas\n2 - Bosques\n3 - Fonte de rios doces.");
+                res = Convert.ToChar(Console.ReadLine());
+                switch (res)
                 {
-                    Console.Clear();
-                    Console.WriteLine("Esse deus é ÉREBO.");
+                    case '1':
+                        Console.Clear();
+                        Console.WriteLine("Esse deus é PAN.");
+                        break;
+
+                    case '2':
+                        Console.Clear();
+                        Console.WriteLine("Esse deus é SÁTIROS.");
+                        break;
+
+                    case '3':
+                        Console.Clear();
+                        Console.WriteLine("Esse deus é NÁIADES.");
+                        break;
                 }
-            }
-        }
-        else if(res == '2')
-        {
-            Console.Clear();
-            Console.WriteLine("Esse deus esta mais associado à:\n\n1 - Florestas e montanhas\n2 - Bosques\n3 - Fonte de rios doces.");
-            res = Convert.ToChar(Console.ReadLine());
-            if(res == '1')
-            {
+                break;
+
+            case '3':
                 Console.Clear();
-                Console.WriteLine("Esse deus é PAN.");
-            }
-            else if(res == '2')
-            {
-                Console.Clear();
-                Console.WriteLine("Esse deus é SÁTIROS.");
-            }
-            else if(res == '3')
-            {
-                Console.Clear();
-                Console.WriteLine("Esse deus é NÁIADES.");
-            }
-        }
-        else if(res == '3')
-        {
-            Console.Clear();
-            Console.WriteLine("Esse Deus esta mais associado ao:\n\n1 - Sol\n2 - Lua\n3 - Aurora");
-            res = Convert.ToChar(Console.ReadLine());
-            if(res == '1')
-            {
-                Console.Clear();
-                Console.WriteLine("Esse Deus é HÉLIOS.");
-            }
-            else if (res == '2')
-            {
-                Console.Clear();
-                Console.WriteLine("Essa deusa é SELENE.");
-            }
-            else if(res == '3')
-            {
-                Console.Clear();
-                Console.WriteLine("Essa deus é EOS.");
-            }
+                Console.WriteLine("Esse Deus esta mais associado ao:\n\n1 - Sol\n2 - Lua\n3 - Aurora");
+                res = Convert.ToChar(Console.ReadLine());
+                switch (res)
+                {
+                    case '1':
+                        Console.Clear();
+                        Console.WriteLine("Esse Deus é HÉLIOS.");
+                        break;
+
+                    case '2':
+                        Console.Clear();
+                        Console.WriteLine("Essa deusa é SELENE.");
+                        break;
+
+                    case '3':
+                        Console.Clear();
+                        Console.WriteLine("Essa deus é EOS.");
+                        break;
+                }
+                break;
         }
     }
-
 }
 else if (res == 'n')
 {
